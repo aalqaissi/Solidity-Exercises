81a9d0ee0aecedb431bd93cf5f610b3596b7fc44
Ayyoub Al-Qaissi <ayyoub.al-qaissi@globitel.com>
2018-08-30 17:59:44 +0300

    First release

--- Auction.sol
+++ Auction.sol
@@ -1,35 +1,77 @@
 pragma solidity ^0.4.0;
+pragma experimental ABIEncoderV2;
 
 contract Auction {
     
+    string public password;
+    address public ownerAccount;
+    uint public duration;
     
-    uint public deployementTime;
-    address public beneficiary;
-    string private password;
     
-    constructor (string asset, address owner) public {
-        deployementTime = now;
-        beneficiary = owner;
-        password = asset;
+    struct Bidder {
+        uint maxBidPrice;
+        address account;
     }
     
-
-    // build bid() function
+    Bidder public maxBidder;
     
-
-
-    // build showMax() function
+    // mapping (address => Student) students;
     
+    // struct Student {
+    //     string name;
+    //     uint age;
+    //     bool isEnjoy;
+    //     address account;
+    // }
     
+    function Auction(string _password, address _ownerAccount, uint _duration) public {
+        password = _password;
+        ownerAccount = _ownerAccount;
+        duration = _duration;
+    }
     
-
-    // build sellAsset() function
-
-
-
-
-    // build getAsset() function
-
+    function showMax () public view returns (Bidder) {
+        return maxBidder;
+    }  
+    
+    function getAsset() public view returns (string) {
+        return password;
+    }
+    
+    // function sellAsset () public () {
+    // }
+    
+    // function sellAsset () public () {
+    // }
+    
+/*
+    function addStudent(Student _student) public {
+        var student = students[_student.account];
+        
+        student.name = _student.name;
+        student.age = _student.age;
+        student.isEnjoy = _student.isEnjoy;
+        student.account = _student.account;
+        
+        
+        
+        // student = Student(_student.name,_student.age, _student.isEnjoy, _student.account);
+        // students.push(_student.account, student);
+    }
+    
+    function getStudent(address account) public view returns (Student) {
+        return students[account];
+        //return students.get(account);
+    }
+    
+    function getStudents() public view returns (Student[]) {
+       //return students;
+    }
+    
+    function countStudents() public view returns (string) {
+        //return students.length();
+    }
+*/    
     
     
 }
--- BCCamp.sol
+++ BCCamp.sol
@@ -1,31 +1,46 @@
-pragma solidity ^0.4.18;
+pragma solidity ^0.4.0;
+pragma experimental ABIEncoderV2;
 
 contract BCCamp {
-    
-// Declare variables that will store information of BlockChain mini boot camp students, this information includes name, age, 
-// and if he/she is enjoying or not, every student information should be bound with his/her ethereum account address.
-    
-// your code here
 
-
+    Student public student;
     
-    function addStudent(address _address, uint _age, string name, bool _isEnjoying) public {
-       // your code here
+    mapping (address => Student) students;
+    
+    struct Student {
+        string name;
+        uint age;
+        bool isEnjoy;
+        address account;
     }
-
-
-
-    // build getStudents() function
     
+    function addStudent(Student _student) public {
+        var student = students[_student.account];
+        
+        student.name = _student.name;
+        student.age = _student.age;
+        student.isEnjoy = _student.isEnjoy;
+        student.account = _student.account;
+        
+        
+        
+        // student = Student(_student.name,_student.age, _student.isEnjoy, _student.account);
+        // students.push(_student.account, student);
+    }
     
-
-    // build getStudent(address _address) function
+    function getStudent(address account) public view returns (Student) {
+        return students[account];
+        //return students.get(account);
+    }
     
-
-
-
-    // build countStudents() function
-
+    function getStudents() public view returns (Student[]) {
+       //return students;
+    }
+    
+    function countStudents() public view returns (string) {
+        //return students.length();
+    }
+    
     
     
 }
